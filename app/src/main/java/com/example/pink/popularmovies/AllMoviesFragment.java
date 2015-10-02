60cef4ee130cb0740ab47084c029cd8f513dab21
pinky <opinkymail@gmail.com>
2015-10-02 00:05:12 -0400

    Query and display movie id and its poster art.

--- app/src/main/java/com/example/pink/popularmovies/AllMoviesFragment.java
+++ app/src/main/java/com/example/pink/popularmovies/AllMoviesFragment.java
@@ -1,29 +1,30 @@
 package com.example.pink.popularmovies;
 
-import android.app.Activity;
+import android.content.SharedPreferences;
 import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.GridLayout;
 import android.widget.GridView;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -39,7 +40,8 @@ public class AllMoviesFragment extends Fragment {
     /**
      * Adapter linking all movies data to its grid list view.
      */
-    private ArrayAdapter<String> mAllMoviesAdapter;
+    private CustomImageListAdapter mAllMoviesAdapter;
+    private String[] mMovieIds = new String[0];
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -77,8 +79,16 @@ public class AllMoviesFragment extends Fragment {
 
     @Override
     public void onStart() {
-
         super.onStart();
+        fetchMovies();
+    }
+
+    private void fetchMovies() {
+        FetchAllMoviesTask moviesTask = new FetchAllMoviesTask();
+        // TO DO: Read sort_by setting from preferences.
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String sortBy = sharedPref.getString("sortby", "popularity.desc");
+        moviesTask.execute(sortBy);
     }
 
     @Override
@@ -109,15 +119,17 @@ public class AllMoviesFragment extends Fragment {
         Log.i(LOG_TAG, "onCreateView");
 
         // TO DO: Query for movie titles and IDs? to get poster.
-        CustomImageListAdapter adapter = new CustomImageListAdapter(getActivity(),
-                new String []{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"});
+        mAllMoviesAdapter = new CustomImageListAdapter(getActivity(),
+//                mMovieIds);
+//                new String []{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"});
+                new ArrayList<String>());
 // ListView Implementation
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 //        ListView listViewAllMovies = (ListView) rootView.findViewById(R.id.fragment_main_listview);
 //        listViewAllMovies.setAdapter(adapter);
 // GridView implementation
         GridView gridViewAllMovies = (GridView) rootView.findViewById(R.id.fragment_main_gridview);
-        gridViewAllMovies.setAdapter(adapter);
+        gridViewAllMovies.setAdapter(mAllMoviesAdapter);
 
         return rootView;
     }
@@ -170,7 +182,159 @@ public class AllMoviesFragment extends Fragment {
         protected String[] doInBackground(String... params) {
             // http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ec842fdd2a58bc4d60d0e08a6576cb52
             // TO DO: Use my API key
-            return new String[0];
+            String[] popularMovies = null;
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String popularMoviesJsonString = null;
+
+            String format = "json";
+            String sortBy = "popularity.desc";
+            String apiKey = "ec842fdd2a58bc4d60d0e08a6576cb52";
+            try {
+                // Construct the URL for the api.themoviedb.org query
+                // Possible parameters are avaiable at API page, at
+                // http://docs.themoviedb.apiary.io/#reference/configuration/configuration/get?console=1
+                final String POPULARMOVIES_BASE_URL =
+                        "http://api.themoviedb.org/3/discover/movie?";
+                final String SORT_BY_PARAM = "sort_by";
+                final String API_KEY_PARAM = "api_key";
+                Uri builtUri = Uri.parse(POPULARMOVIES_BASE_URL).buildUpon()
+                        .appendQueryParameter(SORT_BY_PARAM, params[0])
+                        .appendQueryParameter(API_KEY_PARAM, apiKey)
+                        .build();
+//                URL url = new URL("http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ec842fdd2a58bc4d60d0e08a6576cb52");
+                URL url = new URL(builtUri.toString());
+                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
+                // Create the request to themoviedb, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                popularMoviesJsonString = buffer.toString();
+                //Log.v(LOG_TAG, "Forecast JSON String: " + forecastJsonStr);
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the popular movies data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            // Retrieve popular movies data.
+            try {
+//                List<String> listMovies = getPopularMoviesIdsFromJson(popularMoviesJsonString);
+//                String[] result = new String[listMovies.size()];
+//                result = listMovies.toArray(result);
+//                return result;
+                String[] result = getPopularMoviesIdsFromJson(popularMoviesJsonString);
+                mMovieIds = result;
+                return result;
+            } catch (JSONException je) {
+                Log.e(LOG_TAG, "Error", je);
+            }
+            return null;
+
+            //return new String[0];
+        }
+
+        /**
+         * Take the String representing the complete forecast in JSON Format and
+         * pull out the data we need to construct the Strings needed for the wireframes.
+         *
+         * Fortunately parsing is easy:  constructor takes the JSON string and converts it
+         * into an Object hierarchy for us.
+         */
+        private String[] getPopularMoviesIdsFromJson(String popularMoviesJsonStr)
+//        private List<String> getPopularMoviesIdsFromJson(String popularMoviesJsonStr)
+                throws JSONException {
+
+            // These are the names of the JSON objects that need to be extracted.
+            final String MOVIEDB_ID = "id";
+            final String MOVIEDB_RESULTS = "results";
+            final String MOVIEDB_POSTER_PATH = "poster_path";
+            final String MOVIEDB_TOTAL_RESULTS = "total_results";
+
+            JSONObject forecastJson = new JSONObject(popularMoviesJsonStr);
+            JSONArray moviesArray = forecastJson.getJSONArray(MOVIEDB_RESULTS);
+            int totalResults = forecastJson.getInt(MOVIEDB_TOTAL_RESULTS);
+
+            // themoviedb returns popular movies ids based upon the sort by parameter.
+
+            List<String> resultStrs = new ArrayList<String>();
+            String[] result = new String[moviesArray.length()];
+            for(int i = 0; i < moviesArray.length(); i++) {
+                String movieId;
+                String posterPath;
+
+                // Get the JSON object representing the day
+                JSONObject movie = moviesArray.getJSONObject(i);
+
+                movieId = movie.getString(MOVIEDB_ID);
+                posterPath = movie.getString(MOVIEDB_POSTER_PATH);
+//                resultStrs.add(movieId);
+                result[i] = movieId + ", " + posterPath;
+            }
+
+            for (String s : result) {
+//            for (String s : resultStrs) {
+                Log.v(LOG_TAG, "Movie id: " + s);
+            }
+//            return resultStrs;
+//            List<String> listMovies = getPopularMoviesIdsFromJson(popularMoviesJsonString);
+//            String[] result = new String[listMovies.size()];
+//            result = listMovies.toArray(result);
+//            String[] result = new String[resultStrs.size()];
+//            result = resultStrs.toArray(result);
+            return result;
+        }
+
+        @Override
+        /**
+         * Update the list view with the background return strings of live data.
+         */
+        protected void onPostExecute(String[] result) {
+            if (result != null) {
+                mAllMoviesAdapter.clear();
+                for (String movieIdStr: result) {
+                    mAllMoviesAdapter.add(movieIdStr);
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/pink/popularmovies/CustomImageListAdapter.java
+++ app/src/main/java/com/example/pink/popularmovies/CustomImageListAdapter.java
@@ -10,45 +10,58 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+
 /**
  * Created by pink on 9/28/2015.
  */
 public class CustomImageListAdapter extends ArrayAdapter<String> {
 
     private final Activity context;
-    private final String[] itemname;
-//    private final Integer[] imgid;
+//    private final String[] itemname;
+    private final ArrayList<String> mMovieData;
 
-    public CustomImageListAdapter(Activity context, String[] itemname) {
-//        public CustomListAdapter(Activity context, String[] itemname, Integer[] imgid) {
-        super(context, R.layout.list_movie_item, itemname);
+    public CustomImageListAdapter(Activity context, ArrayList<String> movieData) {
+//    public CustomImageListAdapter(Activity context, String[] itemname) {
+        super(context, R.layout.list_movie_item, movieData);
+//        super(context, R.layout.list_movie_item, itemname);
         // TODO Auto-generated constructor stub
 
         this.context = context;
-        this.itemname = itemname;
-//            this.imgid = imgid;
+        this.mMovieData = movieData;
+//        this.itemname = itemname;
     }
 
     public View getView(int position, View view, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
         View rowView = inflater.inflate(R.layout.list_movie_item, null, true);
 
+        String movieData = mMovieData.get(position);
+        int idxComma = movieData.indexOf(",");
+
         TextView txtTitle = (TextView) rowView.findViewById(R.id.all_list_title);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.all_list_poster);
-        setImage(imageView);
+        String posterPath = movieData.substring(idxComma+2);
+        setImage(imageView, posterPath);
 
-        txtTitle.setText(itemname[position]);
-//        imageView.setImageResource(imgid[position]);
+        String text = movieData.substring(0,idxComma);
+        txtTitle.setText(text);
+//        txtTitle.setText(itemname[position]);
         return rowView;
 
     }
 
-    private void setImage(ImageView imgMovie) {
-        // TO DO: Look up poster for movie title.
+//    private String getImageName(String movieId) {
+//
+//    }
+
+    private void setImage(ImageView imgMovie, String aPosterPath) {
+        // TO DO: Look up poster for movie id.
         Picasso.with(
                 context)
-                .load("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg")
-                .resize(100, 100)
+                .load("http://image.tmdb.org/t/p/w185/" + aPosterPath)
+//                .load("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg")
+                        .resize(100, 100)
                 .into(imgMovie);
     }
 }
